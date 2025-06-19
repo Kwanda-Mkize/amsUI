a861a8fc3d1958d17ff22925a8db78a82737f59f
Kwanda-Mkize <kwanda.mkize@exazeit.com>
2025-06-19 21:55:41 +0200

    Initial-UI-folder-structure

---
+++ src/app/components/dashboard.side.nav/dashboard.side.nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard.side.nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard.side.nav.component.html',
+  styleUrl: './dashboard.side.nav.component.css'
+})
+export class DashboardSideNavComponent {
+
+}
