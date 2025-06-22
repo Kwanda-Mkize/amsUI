93bca676fd31881a4d70948693c6000710944a29
Kwanda-Mkize <kwanda.mkize@exazeit.com>
2025-06-22 22:52:40 +0200

    updated  main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { appConfig } from "./app/app.config";
 import { AppComponent } from "./app/app.component";
 import { msalInstance } from "./app/msal.config";
 
-// bootstrapApplication(AppComponent, appConfig)
-//   .catch((err) => console.error(err));
 async function main() {
   await msalInstance.initialize();
   await bootstrapApplication(AppComponent, appConfig);
