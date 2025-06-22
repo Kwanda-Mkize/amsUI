d0b76f4c6931cf0461c1e95a6b64afe0eebba186
Kwanda <kwanda.mkize@exazeit.com>
2025-06-22 11:57:54 +0200

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   baseUrl: "https://localhost:7091/api/v1",
-  clientId: "16684db4-e91f-44ac-87ab-42b6455b84cb",
-  tenantId: "ceecc926-0c8f-48d1-b01b-b17091c7d7f5",
+  clientId: "",
+  tenantId: "",
 };
